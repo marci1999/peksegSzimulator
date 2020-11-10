d3c1c93b25a83f9cb29b4a9e82a10b01935e53f3
jakus marci <jakus.marton@gmail.com>
2020-11-10 13:03:52 +0100

    osztály2

--- peksegSzimulator/Pekseg.cs
+++ peksegSzimulator/Pekseg.cs
@@ -8,6 +8,17 @@ namespace peksegSzimulator
 {
     class Pekseg
     {
+        string nev;
+        List<Pekaru> aruk = new List<Pekaru>();
+        DateTime alapitva;
 
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Alapitva { get => alapitva;}
+        internal List<Pekaru> Aruk { get => aruk;}
+
+        public override string ToString()
+        {
+            return string.Format("\"" + this.nev + " - " + this.alapitva+ "\"");
+        }
     }
 }
